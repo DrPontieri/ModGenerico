3d53510e83c487422511b1dfa42b4a61dd87e76b
DrPontieri <drpontieri@outlook.com.br>
2023-06-25 20:57:20 -0300

    CadastroPost

--- CadastroApp/src/app/cadastro-details/cadastro-details-form/cadastro-details-form.component.ts
+++ CadastroApp/src/app/cadastro-details/cadastro-details-form/cadastro-details-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { CadastroDetails } from 'src/app/shared/cadastro-details.model';
 import { CadastroDetailsService } from 'src/app/shared/cadastro-details.service';
+
+
 
 
 @Component({
@@ -11,17 +15,24 @@ import { CadastroDetailsService } from 'src/app/shared/cadastro-details.service'
 })
 
 export class CadastroDetailsFormComponent implements OnInit {
-  isValidFormSubmitted = false;
-  validateEmail = true;
-  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
-  CadastroDetails = new CadastroDetails();
-  constructor(public service:CadastroDetailsService){
 
-  }
+constructor(public service:CadastroDetailsService){
 
-  ngOnInit(): void {
-  }
-  onFormSubmit(form: CadastroDetails) {
-    this.service.formData.email = this.emailPattern;
- }
+}
+
+ngOnInit(): void {
+
+}
+
+onSubmit(form:NgForm){
+  const myobservable = this.service.PostPessoa();
+
+  const myObserver={
+    error:(err:Error) => console.error('Observer got an error: ' + err),
+    complete: () => console.log('Observer got a complete notification'),
+  };
+
+  myobservable.subscribe(myObserver);
+};
+
 }
